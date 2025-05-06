2eab725b0591fc637573f5fd837a177f7de83bf4
FIC <fic@134>
2025-05-05 21:53:48 -0300

    Iniciui projeto 05/05

---
+++ Context/LivrosContext.cs
@@ -0,0 +1,159 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ProjetoLivros.Models;
+
+namespace ProjetoLivros.Context
+{
+    public class LivrosContext : DbContext
+    {
+        // Cada tabela -> DbSet
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<TipoUsuario> TipoUsuarios { get; set; }
+        public DbSet<Assinatura> Assinaturas { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public LivrosContext(DbContextOptions<LivrosContext>options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // String de Coneccao 
+            optionsBuilder.UseSqlServer("Data Source=NOTE16-S28\\SQLEXPRESS;Initial Catalog=Livros;User Id=sa;Password=Senai@134;TrustServerCertificate=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>(
+                entity =>
+                // Representação da tabela
+                {
+                    // Primary Key
+                    entity.HasKey(u => u.UsuarioId);
+
+                    entity.Property(u => u.NomeCompleto)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                    entity.Property(u => u.Email)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                    // Email e unico
+                    entity.HasIndex(u => u.Email)
+                    .IsUnique();
+
+                    entity.Property(u => u.Senha)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                    entity.Property(u => u.Telefone)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                    entity.Property(u => u.DataCadastro)
+                    .IsRequired();
+
+                    entity.Property(u => u.DataAtualizacao)
+                    .IsUnicode(false);
+
+                    entity.HasOne(u => u.TipoUsuario)
+                    .WithMany(t => t.Usuarios)
+                    .HasForeignKey(u => u.TipoUsuarioId)
+                    .OnDelete(DeleteBehavior.Cascade);
+                });
+        
+
+            modelBuilder.Entity<TipoUsuario>(entity =>
+            {
+                // Configuro a primaryKey
+                entity.HasKey(t => t.TipoUsuarioId);
+
+                // Vou campo a campo configurando
+                entity.Property(t => t.DescricaoTipo)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+                // Descricao nao pode se repitir
+                entity.HasIndex(t => t.DescricaoTipo)
+                .IsUnique();
+            });
+
+            modelBuilder.Entity<Livro>(entity =>
+            {
+                entity.HasKey(l => l.LivroId);
+
+                entity.Property(l => l.Titulo)
+                .IsRequired()
+                .HasMaxLength(200)
+                .IsUnicode(false);
+
+                entity.Property(l => l.Autor)
+                .IsRequired()
+                .HasMaxLength(200)
+                .IsUnicode(false);
+
+                entity.Property(l => l.Descricao)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+                entity.Property(l => l.DataPublicacao)
+                .IsRequired();
+
+                // Relacionamento
+                // Livro - Categoria
+                // 1 - N
+                entity.HasOne(l => l.Categoria)
+                .WithMany(c => c.Livros)
+                .HasForeignKey(l => l.CategoriaId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            });
+
+            // Categoria e assinatura
+
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(c => c.CategoriaId);
+
+                entity.Property(c => c.NomeCategoria)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);            
+            
+            });
+
+            modelBuilder.Entity<Assinatura>(
+                entity =>
+                {
+                    entity.HasKey(a => a.AssinaturaId);
+
+                    entity.Property(a => a.DataInicio)
+                    .IsRequired();
+
+                    entity.Property(a => a.DataFim)
+                    .IsRequired();
+
+                    entity.Property(a => a.Status)
+                    .IsRequired()
+                    .HasMaxLength (20)
+                    .IsUnicode(false);
+
+                    // assinatura - usuario
+                    entity.HasOne(a => a.Usuario)
+                    .WithMany()
+                    .HasForeignKey (a => a.UsuarioId)
+                    .OnDelete(DeleteBehavior.Cascade);
+                });
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+using ProjetoLivros.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<LivrosContext>();
+
+var app = builder.Build();
+
+app.Run();
